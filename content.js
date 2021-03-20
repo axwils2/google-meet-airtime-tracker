02989c4ba386dcfa56f08b34c318f778dfab7db4
Austin Wilson <austin@newcoastventures.com>
2021-03-20 13:25:03 -0500

    use id of great grand parent instead of name for object key

--- content.js
+++ content.js
@@ -2,53 +2,75 @@
 let monitoring = false;
 let participants = {};
 
+function totalTalkTime() {
+  var total = Object.values(participants).map(function(participant) {
+    return participant.count
+  }).reduce(function(acc, val) { return acc + val; }, 0);
+
+  return Math.max(total, 1);
+};
+
+function talkPercentageString(count, total) {
+  return ' (' + (count / total * 100).toFixed(2) + '%)'
+};
+
 function startMonitoring() {
-  $divs = $('.Djiqwe .IisKdb').each(function() {
+  $('.Djiqwe .IisKdb').each(function() {
     var nameEl = $(this).parent().next()[0];
 
     if (nameEl) {
-      var name = $(nameEl).text();
-      var participant = participants[name] || {};
-      participant.count = 0;
-      var observer = new MutationObserver(function(mutations, obs) {
-        mutations.forEach(function(mutation) {
-          if (mutation.attributeName === "class") {
-            var target = $(mutation.target)
-            var attributeValue = target.prop(mutation.attributeName);
-            var previousCount = participant['count'] || 0;
-            participant['count'] = previousCount + 1;
-          }
+      var idString = $(this).parent().parent().parent().parent().attr('data-initial-participant-id');
+
+      if (idString) {
+        var idStringParts = idString.split('/')
+        var id = idStringParts[idStringParts.length - 1];
+        var name = $(nameEl).text();
+        var participant = participants[id] || {};
+
+        participant.name = name;
+        participant.count = 0;
+
+        var observer = new MutationObserver(function(mutations, obs) {
+          mutations.forEach(function(mutation) {
+            if (mutation.attributeName === "class") {
+              var target = $(mutation.target)
+              var attributeValue = target.prop(mutation.attributeName);
+              var previousCount = participant['count'] || 0;
+              participant.count = previousCount + 1;
+
+              $(nameEl).text(name + talkPercentageString(participant.count))
+            }
+          });
         });
-      });
 
-      participant['observer'] = observer;
-      participants[name] = participant;
+        participant.observer = observer;
+        participants[id] = participant;
 
-      var config = {
-        attributes: true
-      };
+        var config = {
+          attributes: true
+        };
 
-      observer.observe(this, config);
+        observer.observe(this, config);
+      }
     }
   });
   
-  const names = Object.keys(participants).join(', ');
-  console.log('Monitoring ' + names + '!');
-
+  console.log('Monitoring!');
   monitoring = true;
 };
 
 function stopMonitoring() {
-  var totalTalkTime = Object.values(participants).map(function(participant) {
-    return participant.count
-  }).reduce(function(acc, val) { return acc + val; }, 0);
+  var alertArray = [];
+  var total = totalTalkTime();
 
-  Object.keys(participants).forEach(function(participantName) {
-    var participant = participants[participantName];
-    console.log(participantName + ' talked for roughly ' + Math.ceil(participant.count / 12) + ' seconds (' + (participant.count / totalTalkTime * 100).toFixed(2) + '%)');
+  Object.keys(participants).forEach(function(participantKey) {
+    var participant = participants[participantKey];
+    alertArray.push(participant.name + ' ' + talkPercentageString(participant.count, total));
     participant.observer.disconnect();
   });
 
+  
+  alert(alertArray.join('\n'));
   console.log('Monitoring stopped!');
   monitoring = false;
 }
